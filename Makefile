0ad53051eae76fb5a2905a804b44acf4648e1fb2
Antonio Cuni <anto.cuni@gmail.com>
2017-11-09 01:05:41 +0100

    add a boot script which automatically connects to wifi, if the configuration script is present

--- Makefile
+++ Makefile
@@ -1,3 +1,14 @@
+TTY=/dev/ttyUSB0
+
 # use this to connect to the serial port
+# to exit the screen, ctrl-A k
+# DO NOT USE ctrl-A d, else the tty stays busy
 screen:
-	screen /dev/ttyUSB0 115200
+	screen -S upython $(TTY) 115200
+
+kill-screen:
+	@if screen -ls | grep -q upython; then screen -X -S upython quit; fi
+
+boot: kill-screen
+	ampy -p $(TTY) put boot.py
+	if [ -f wifi.ini ]; then ampy -p $(TTY) put wifi.ini; fi
---
+++ boot.py
@@ -0,0 +1,56 @@
+# This file is executed on every boot (including wake-boot from deepsleep)
+#import esp
+#esp.osdebug(None)
+import gc
+import webrepl
+webrepl.start()
+gc.collect()
+
+def connect_wifi(ssid, password, timeout=10):
+    import time
+    import network
+    sta_if = network.WLAN(network.STA_IF)
+    if sta_if.isconnected():
+        print('wlan already connected')
+        print('network config:', sta_if.ifconfig())
+        return
+
+    print('Scanning for available Wi-Fi networks...')
+    ssids = [tup[0] for tup in sta_if.scan()]
+    print(len(ssids), 'networks found:')
+    for name in ssids:
+        print('   ', name)
+    print()
+    #
+    if ssid.encode('latin1') not in ssids:
+        print('SSID %s not found' % ssid)
+        return
+    #
+    print('connecting to %s...' % ssid)
+    sta_if.active(True)
+    timeout *= 1000 # convert from seconds to ms
+    start_time = time.ticks_ms()
+    sta_if.connect(ssid, password)
+    while not sta_if.isconnected():
+        cur_time = time.ticks_ms()
+        if time.ticks_diff(cur_time, start_time) > timeout:
+            print('connection timed out, giving up')
+            sta_if.disconnect()
+            return
+    print('network config:', sta_if.ifconfig())
+
+def auto_connect():
+    try:
+        with open('wifi.ini') as f:
+            content = f.read().strip()
+    except IOError:
+        print('cannot read wifi.ini, skipping Wi-Fi connection')
+    else:
+        ssid, password = content.split(' ')
+        print('Wi-Fi auto-connect: %s' % ssid)
+        connect_wifi(ssid, password)
+
+# if you want to enable Wi-Fi auto-connect, simply put a wifi.ini with only
+# one line
+#    ssid password
+auto_connect()
