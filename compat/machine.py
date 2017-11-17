563cf74da0e4430e1878aa7b5d7c6d104aa3bf8a
Antonio Cuni <anto.cuni@gmail.com>
2017-11-17 01:22:18 +0100

    not really useful so far, but commit the first draft of a compat library to allow running/testing the scripts directly on the PC

---
+++ compat/machine.py
@@ -0,0 +1,18 @@
+class ADC(object):
+
+    def __init__(self, n):
+        pass
+
+    def read(self):
+        return 512
+
+class Pin(object):
+
+    OUT = 'out'
+
+    def __init__(self, n, mode):
+        pass
+
+    def __call__(self, n):
+        pass
+
