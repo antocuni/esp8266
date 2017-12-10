c77533244ac465f4eca4f3413c5a78a7cb099f94
Antonio Cuni <anto.cuni@gmail.com>
2017-12-10 00:13:19 +0000

    improve the auto-connection script and make it working on my sonoff device: in particular, I need to call sta_if.active before scan(), and time.sleep.ms inside the busy loop. While at it, print a message every second while waiting for the network

--- boot.py
+++ boot.py
@@ -19,9 +19,9 @@ def connect_wifi(ssid, password, timeout=10):
         blink(0)
 
 def _connect_wifi(ssid, password, timeout):
-    import time
     import network
     sta_if = network.WLAN(network.STA_IF)
+    sta_if.active(True)
     if sta_if.isconnected():
         print('wlan already connected')
         print('network config:', sta_if.ifconfig())
@@ -39,19 +39,32 @@ def _connect_wifi(ssid, password, timeout):
         print('SSID %s not found' % ssid)
         return
     #
-    print('connecting to %s...' % ssid)
+    sta_if.connect(ssid, password)
+    if _wait_for_connection(sta_if, ssid, timeout):
+        print('network config:', sta_if.ifconfig())
+
+def _wait_for_connection(sta_if, ssid, timeout):
+    import time
     blink(2)
-    sta_if.active(True)
     timeout *= 1000 # convert from seconds to ms
     start_time = time.ticks_ms()
-    sta_if.connect(ssid, password)
+    next_print = start_time
+    i = 0
     while not sta_if.isconnected():
+        # this sleep seems to be necessary on my sonoff device. On the other
+        # hand, on the wemos D1 it connected even without it, no clue why
+        time.sleep_ms(10)
         cur_time = time.ticks_ms()
-        if time.ticks_diff(cur_time, start_time) > timeout:
+        if time.ticks_diff(cur_time, start_time) >= timeout:
             print('connection timed out, giving up')
             sta_if.disconnect()
-            return
-    print('network config:', sta_if.ifconfig())
+            return False
+        if time.ticks_diff(cur_time, next_print) >= 0:
+            i += 1
+            next_print = time.ticks_add(cur_time, 1000)
+            print('connecting to %s... [%d]' % (ssid, i))
+    return True
+
 
 def auto_connect():
     try:
