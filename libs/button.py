e2150c12996cfd02a9035855237da199d62423eb
Antonio Cuni <anto.cuni@gmail.com>
2017-12-10 01:07:27 +0000

    bah, make sure to call the user-defined callback in case we pass one

--- libs/button.py
+++ libs/button.py
@@ -4,12 +4,12 @@ class Button(object):
 
     def __init__(self, pin, on_click=None):
         self.pin = machine.Pin(pin, machine.Pin.IN, machine.Pin.PULL_UP)
-        self.pin.irq(trigger=machine.Pin.IRQ_FALLING, handler=self.on_click)
         if on_click:
             self.on_click = on_click
+        self.pin.irq(trigger=machine.Pin.IRQ_FALLING, handler=self.on_click)
 
     def on_click(self, pin):
-        pass
+        print("Default button handler: %s" % pin)
 
 
 if __name__ == '__main__':
