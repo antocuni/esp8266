6f09ead1ad38ffd2be6a8fe6a0ccd43fbc293941
Antonio Cuni <anto.cuni@gmail.com>
2017-12-11 22:05:20 +0000

    add a comment

--- libs/sonoff.py
+++ libs/sonoff.py
@@ -5,6 +5,7 @@
 # to flash the firmware using the "-fm dout" option:
 # https://forum.micropython.org/viewtopic.php?f=16&t=3777
 #
+#   esptool.py --port /dev/ttyUSB0 erase_flash
 #   esptool.py --port /dev/ttyUSB0 write_flash -fs 1MB -fm dout 0x0 esp8266-20170823-v1.9.2.bin
 
 RELAY = 12
