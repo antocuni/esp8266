424b57cfcbba59b5f80bf5bf5d6f40f061b567fc
Antonio Cuni <anto.cuni@gmail.com>
2017-12-11 22:40:31 +0000

    make it possible to toggle the switch by pressing the button

--- smartplug/main.py
+++ smartplug/main.py
@@ -9,7 +9,7 @@ frozen modules:
   - mqtt_as [https://github.com/peterhinch/micropython-mqtt]
 """
 
-from sonoff import LED, BUTTON
+from sonoff import LED, BUTTON, RELAY
 from machine import Pin, Signal
 import uasyncio as asyncio
 from aswitch import Pushbutton
@@ -18,16 +18,35 @@ LOOP = asyncio.get_event_loop()
 
 class SmartPlug(object):
 
-    led = Signal(Pin(LED, Pin.OUT), invert=True)
+    led = Signal(Pin(LED, Pin.OUT, value=0), invert=True)
+    relay = Signal(Pin(RELAY, Pin.OUT, value=0))
     button = Pushbutton(Pin(BUTTON, Pin.IN))
 
     def __init__(self):
         self.button.press_func(self.on_click)
         self.button.long_func(self.on_long_press)
         self.button.double_func(self.on_double_click)
+        self.status = False
+
+    @property
+    def status(self):
+        """
+        The status of the switch. The invariant is that the following are always
+        in sync:
+          - the status led
+          - the relay
+        """
+        return self._status
+
+    @status.setter
+    def status(self, v):
+        print ("setting status to", v)
+        self._status = v
+        self.led.value(v)
+        self.relay.value(v)
 
     def on_click(self):
-        print('click')
+        self.status = not self.status
 
     def on_long_press(self):
         print('long press')
